2a4a698cf28657928c755e6657cac61a0ff72354
messi-wpy <904315105@qq.com>
2019-09-09 23:52:09 +0800

    create: main_activity布局

---
+++ app/src/main/java/com/example/trending/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.trending;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mToolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("");
+
+
+
+    }
+}
